2b3667d1a741b8e30a30eefbc81add3fa641a82c
Danie K <41302248+elledkaye@users.noreply.github.com>
2024-08-13 09:32:03 -0400

    first commit
    First to repo

---
+++ GroceryListApp/GroceryListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryListAppApp.swift
+//  GroceryListApp
+//
+//  Created by Danielle Kaye on 8/13/24.
+//
+
+import SwiftUI
+
+@main
+struct GroceryListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
