308c2760f2d482c964b972fe2c24a669a901a3b5
Danie K <41302248+elledkaye@users.noreply.github.com>
2024-08-29 21:22:21 -0400

    Home view model
    The home view model is set up to handle fetching the data from the firebase store. Added some minor error handling.

--- GroceryListApp/ViewModels/HomeViewModel.swift
+++ GroceryListApp/ViewModels/HomeViewModel.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+import SwiftUI
+import FirebaseFirestore
+
+class HomeViewModel: ObservableObject{
+    @Published var groceryLists: [String] = [] // Array of string to store the names of grocery lists
+    @Published var errorMessage : String = ""
+    
+    private let db = Firestore.firestore()
+    
+    
+    // Call a functio that will fetch the current users grocery list
+    func fetchGroceryLists(for userId: String){
+        db.collection("users").document(userId).collection("groceryLists").getDocuments{[weak self] snapshot, error in
+            if let error = error{
+                self?.errorMessage = "Failed to getch lists: \(error.localizedDescription)"
+                return
+                
+            }
+            
+            if let documents = snapshot?.documents{
+                self?.groceryLists = documents.compactMap{ $0["name"] as? String }
+            }
+            
+        }
+        
+        
+        
+    }
+    
+    
+    
+}
