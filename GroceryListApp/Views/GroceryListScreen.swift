62c746fe4a6f482dec1d96735bdd0fb5f28b42b5
Danie K <41302248+elledkaye@users.noreply.github.com>
2024-11-14 09:27:31 -0500

    GroceryListScreen UI Styling
    Updated UI on groceryListScreen

--- GroceryListApp/Views/GroceryListScreen.swift
+++ GroceryListApp/Views/GroceryListScreen.swift
@@ -1,8 +1,8 @@
 /*
--> Use a Modal to display CreateGroceryList ( User will enter name of a new list)
--> Plus button will show an alert to create a new grocery list, remove button
--> item: Name, and the listName which the item belongs to
-
+ -> Use a Modal to display CreateGroceryList ( User will enter name of a new list)
+ -> Plus button will show an alert to create a new grocery list, remove button
+ -> item: Name, and the listName which the item belongs to
+ 
  Workflow to creating a grocery list:
  User clicks the '+' sign to create new list -> Modal displays prompting user to enter in title of the list and a couple of list items -> User clicks 'Save button' -> When user clicks 'Save' the newly created list gets created and stored in firebase DB which will reflect on the home screen
  */
@@ -29,43 +29,64 @@ struct GroceryListScreen: View {
     // State which tracks whether the detail view is displayed or not
     @State var isPresentedDetailView: Bool = false
     
-
+    
     var body: some View {
         NavigationView {
             ZStack {
-                Color.blue.opacity(0.3)
+                LinearGradient(gradient: Gradient(colors: [.green, .white]), startPoint: .top, endPoint: .bottom)
+                    .opacity(0.3)
                     .ignoresSafeArea()
+                    .cornerRadius(10)
+                
                 
                 // If user has not grocery list display "No Grocery Lists found"
-                VStack {
+                VStack(spacing:20) {
                     if viewModel.userGroceryLists.isEmpty {
                         Text("No grocery lists found")
                             .font(.headline)
-                            .foregroundColor(.gray)
+                            .foregroundColor(.white)
+                        
                     }
-                    
-                    
                     // else display the list of lists
-                        // if the user selects a list we then want to open the detailModal
-                        // within the detailModal we display the list of items
-                    
-            
+                    // if the user selects a list we then want to open the detailModal
+                    // within the detailModal we display the list of items
                     else {
                         List(viewModel.userGroceryLists) { groceryList in
                             // Each grocery list name is wrapped in a Button that calls openGroceryList
                             Button(action:{
                                 openGroceryList(groceryList)
                             }){
+                                HStack{
+                                    // Display name of each grocery list
+                                    Text(groceryList.groceryListName)
+                                        
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color.black.opacity(0.2), radius: 5, x: 0, y: 2)
+                                        .padding()
+                                    
+                                    
+                                }
+                                .frame(maxWidth: .infinity, alignment: .leading)
+                                .padding(.vertical, 10)
+                                .background(Color.white.opacity(0.08))
+                                .cornerRadius(10)
+                                .overlay(
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(Color.white, lineWidth: 1)
+                                )
                                 
-                                // Display name of each grocery list
-                                Text(groceryList.groceryListName)
-                                  
                             }
-                        }
-                    }
+                            .listStyle(.automatic)
+                            .listRowBackground(Color.gray)
+                            .shadow(color: Color.black.opacity(0.2), radius: 2, x: 0, y: 2)
+                        } // End of List
+                        
+                    } // End of else
                     
+                  
                 } // End of Outer VStack
-                
+                .cornerRadius(10)
+                .shadow(color: Color.black.opacity(0.2), radius: 2, x: 0, y: 2)
                 .padding()
                 
             } // End of ZStack
@@ -73,17 +94,17 @@ struct GroceryListScreen: View {
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button(action: {
                         
-                       // Resetting so when user clicks "+" sign all fields are cleared
-                       newGroceryListName.groceryListName = ""
+                        // Resetting so when user clicks "+" sign all fields are cleared
+                        newGroceryListName.groceryListName = ""
                         newGroceryItems = []
                         // Display create grocery list model
                         isPresentedCreateGroceryList = true
-        
+                        
                     }) {
                         Image(systemName: "plus")
                     }
                 }
-
+                
                 ToolbarItem(placement: .navigationBarLeading) {
                     Button(action: {
                         // Action goes here
@@ -130,7 +151,7 @@ struct GroceryListScreen: View {
         isPresentedDetailView = true
     }
     
-   
+    
 }
 
 
